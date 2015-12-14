8d01a52f7820910cc5eea4f8085507d708ad3c5e
Matan Lurey <matan@lurey.org>
2015-12-14 18:06:20 +0000

    Add better class documentation.

--- lib/src/clazz/built_class.dart
+++ lib/src/clazz/built_class.dart
@@ -13,16 +13,45 @@ class BuiltClass {
   static final Expando<int> _hashCodes = new Expando<int>('hashCodes');
   static const _listEquals = const ListEquality();
 
+  /// Whether this class should be marked `abstract`.
   final bool isAbstract;
+
+  /// Whether this class should be marked `external`.
   final bool isExternal;
+
+  /// The name of the class.
   final String name;
 
+  /// Class-level member fields.
   final List<BuiltVariable> fields;
+
+  /// Class-level member methods.
+  ///
+  /// To implement constructors, use the [name] of the class as the method name
+  /// or with the `.name` syntax for named constructors, i.e.:
+  ///     const BuiltClass(
+  ///         'Foo',
+  ///         methods: const [const BuiltMethod(name: 'Foo.fromBar')])
+  ///
+  /// TODO: Consider a seperate collection for constructors and add missing
+  /// parts like `const` for a constructor.
   final List<BuiltMethod> methods;
 
+  /// Generic types the class supports.
+  ///
+  /// Example use to create `class Foo<T>`:
+  ///     const BuiltClass(
+  ///         'Foo',
+  ///         generics: const [const BuiltType('T')]);
   final List<BuiltType> generics;
+
+  /// Which class to extend, if any.
   final BuiltType extend;
+
+  /// Which classes to implement, if any.
   final List<BuiltType> implement;
+
+  /// Which classes to mixin, if any.
   final List<BuiltType> mixin;
 
   const BuiltClass(this.name,
