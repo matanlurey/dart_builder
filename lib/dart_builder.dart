6d8cb518379c47086c452bdb5429b98aab4faba0
Matan Lurey <matan@lurey.org>
2015-12-24 21:32:04 +0000

    Expose more classes in dart_builder.dart.

--- lib/dart_builder.dart
+++ lib/dart_builder.dart
@@ -2,12 +2,17 @@ library dart_builder;
 
 export 'package:dart_builder/src/clazz/built_class.dart';
 export 'package:dart_builder/src/clazz/built_constructor.dart';
+export 'package:dart_builder/src/clazz/class_builder.dart';
+export 'package:dart_builder/src/clazz/constructor_builder.dart';
 export 'package:dart_builder/src/file/built_directive.dart';
 export 'package:dart_builder/src/file/built_file.dart';
 export 'package:dart_builder/src/method/built_method.dart';
 export 'package:dart_builder/src/method/built_method_body.dart';
 export 'package:dart_builder/src/method/built_method_invocation.dart';
 export 'package:dart_builder/src/parameter_list/built_parameter_list.dart';
+export 'package:dart_builder/src/parameter_list/parameter_list_builder.dart';
 export 'package:dart_builder/src/source_writer.dart';
 export 'package:dart_builder/src/type/built_type.dart';
+export 'package:dart_builder/src/type/type_builder.dart';
 export 'package:dart_builder/src/variable/built_variable.dart';
+export 'package:dart_builder/src/variable/variable_builder.dart';
